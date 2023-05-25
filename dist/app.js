c65c8d9cea6db2d83f1dd5150226b52e82d26e6d
MenasheMtku <menashemetuko@gmail.com>
2023-05-25 09:51:21 +0300

    seection: projects / contact etc added

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,59 @@
-const navigation = document.querySelector(".navbar");
+const navigation = document.querySelector(".header");
 const navigationHeight = navigation.offsetHeight;
+//
+const tabs = document.querySelectorAll(".header-nav-tab");
+//
+const mobileNavBarIcon = document.getElementById("mobile-nav-bar-icon");
+const headerNavList = document.getElementById("header-nav-list");
+let active = document.getElementsByClassName(".active");
 
-document.documentElement.style.setProperty(
-  "--scroll-padding",
-  navigationHeight + "px"
-);
+// media query
+const mediaQuery = window.matchMedia("(max-width: 955px)");
+mediaQuery.addListener(handleDeviceChange);
+
+// document.documentElement.style.setProperty(
+//   "--scroll-padding",
+//   navigationHeight + "px"
+// );
+
+mobileNavBarIcon.addEventListener("click", function () {
+  console.log("hamborger clicked");
+});
+// check the device width, adding/removig class '.active' from a tag in the menu
+function handleDeviceChange(e) {
+  if (e.matches) {
+    tabs.forEach(tab => {
+      tab.addEventListener("click", function () {
+        // hides menu after clicking on a link
+        mobileNavBarIcon.className = "fas fa-bars";
+        headerNavList.style.transform = "translateX(200%)";
+        // Add active class to the selected tab
+        tabs.forEach(tab => tab.classList.remove("active"));
+        this.classList.remove("active");
+        console.log("class 'active' removed");
+      });
+    });
+  } else
+    tabs.forEach(tab => {
+      tab.addEventListener("click", function () {
+        // Add active class to the selected tab
+        tabs.forEach(tab => tab.classList.remove("active"));
+        this.classList.add("active");
+        console.log("class 'active' Added");
+      });
+    });
+}
+
+handleDeviceChange(mediaQuery);
+
+mobileNavBarIcon.onclick = function () {
+  if (mobileNavBarIcon.className == "fas fa-bars") {
+    mobileNavBarIcon.className = "fas fa-times";
+    headerNavList.style.display = "flex";
+    headerNavList.style.transform = "translateX(0)";
+  } else {
+    headerNavList.style.display = "none";
+    mobileNavBarIcon.className = "fas fa-bars";
+    headerNavList.style.transform = "translateX(200%)";
+  }
+};
---
+++ dist/projects.js
@@ -0,0 +1,15 @@
+console.log("project js loaded");
+
+const logos = document.querySelector(".language-logos-flex");
+const cards = document.querySelectorAll(".project-card");
+
+// cards.forEach(card => {
+//   card.addEventListener("mouseenter", function () {
+//     console.log("hover start");
+//     logos.style.display = "flex";
+//   });
+//   card.addEventListener("mouseleave", function () {
+//     console.log("hover end");
+//     logos.style.display = "none";
+//   });
+// });

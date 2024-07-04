198a0446fccfa960730c7142436d4efd5fea1e54
MenasheMtku <mmatuko1@gmail.com>
2024-07-04 15:42:31 +0300

    refactor: json data file added

--- dist/projects.js
+++ dist/projects.js
@@ -1,15 +1,51 @@
 console.log("project js loaded");
 
-const logos = document.querySelector(".language-logos-flex");
-const cards = document.querySelectorAll(".project-card");
+// const logos = document.querySelector(".language-logos-flex");
+// const cards = document.querySelectorAll(".project-card");
+const projects_container = document.querySelector(".project-list");
+const skills_container = document.querySelector(".logos");
 
-// cards.forEach(card => {
-//   card.addEventListener("mouseenter", function () {
-//     console.log("hover start");
-//     logos.style.display = "flex";
-//   });
-//   card.addEventListener("mouseleave", function () {
-//     console.log("hover end");
-//     logos.style.display = "none";
-//   });
-// });
+console.log(projects_container);
+
+const response = await fetch("./data.json");
+const data = await response.json();
+const projects = data.projects;
+const skills = data.skills;
+console.log(skills);
+
+// about section
+skills.forEach(skill => {
+  skills_container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="logo-item">
+            <p class="logo-title">${skill.title}</p>
+            <img class="skill-svg" src = ${skill.image} alt=${skill.alt}/>
+          </div>`
+  );
+});
+
+// projects section
+projects.forEach(project => {
+  projects_container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="project">
+              <div class="proj-img-container">
+                <img src="${project.image}" loading="lazy" />
+              </div>
+              <div class="overview">
+                <h3 class="p-title">${project.title}</h3>
+                <p class="p-summary">
+                ${project.description}
+                </p>
+                <div class="visit">
+                  <a href="${project.siteUrl}" target="_blank" class="project-link"
+                    >Visit Website</a
+                  >
+                    <a href="${project.codeUrl}" target="_blank">
+                    <img class="github-svg-link" src = "./assets/skillsLogos/github-svgrepo-com.svg" alt="project_github_link"/>
+                  </a>
+                </div>
+              </div>
+            </div>`
+  );
+});

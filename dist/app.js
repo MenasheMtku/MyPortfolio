b1b3a9a0bb29793ccbca968e63b165379c84e884
MenasheMtku <menashemetuko@gmail.com>
2024-08-17 23:10:45 +0300

    feat: theme switch base on system settings

--- dist/app.js
+++ dist/app.js
@@ -1,44 +1,65 @@
-// let yearEl = document.querySelector(".year");
-
-const navEl = document.querySelector(".nav");
-const headerNavList = document.querySelector(".nav__links");
-const tabs = document.querySelectorAll(".nav__link");
-const stack = document.querySelectorAll(".skill-svg");
-// const cards = document.querySelectorAll(".project");
-// --------------------------------------------------
-
-// cards.forEach(card => {
-//   card.classList.add("card-shadow");
-//   console.log("box shadow added");
-// });
+"use strict";
+const year = document.querySelector(".year");
+const themeToggle = document.getElementById("theme-toogle");
 
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
-console.log(currentYear);
-document.querySelector(
-  ".year"
-).innerHTML = `&copy; ${currentYear} Menashe Mtku`;
+year.innerHTML = `&copy; ${currentYear} Menashe Mtku`;
+console.log(`${currentYear}\n${year.textContent}`);
 
-const headerEl = document.querySelector("header");
-let navHeight = navEl.getBoundingClientRect().height;
-console.log(navHeight);
-
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  // console.log(entry);
-  if (!entry.isIntersecting) navEl.classList.add("fixed");
-  else navEl.classList.remove("fixed");
+// Theme Handling
+const calculateSettingAsThemeString = ({
+  localStorageTheme,
+  systemSettingDark,
+}) => {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+  return "light";
 };
 
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0.3,
-  rootMargin: `-${navHeight}px`,
+const updateButton = ({ buttonEl, isDark }) => {
+  const srcSvg = isDark
+    ? "./assets/sun-svgrepo-com.svg"
+    : "./assets/moon-svgrepo-com-2.svg";
+  buttonEl.src = srcSvg;
+  // console.log(`Theme toggle button:\n ${buttonEl.src}`);
+};
+
+const updateThemeOnHtmlEl = ({ theme }) => {
+  document.querySelector("html").setAttribute("data-theme", theme);
+};
+/**
+ * On page load:
+ * 1. Grab what we need from the DOM and system settings on page load
+ */
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+/**
+ * 2. Work out the current site settings
+ */
+let currentThemeSetting = calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
 });
+/**
+ * 3. Update the theme setting and button text accoridng to current settings
+ */
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+/**
+ * 4. Add an event listener to toggle the theme
+ */
+button.addEventListener("click", event => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
-headerObserver.observe(headerEl);
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
 
-// console.log(stack);
-// stack.forEach(skill => {
-//   skill.classList.add("btt-box-shadow");
-// });
+  currentThemeSetting = newTheme;
+});
